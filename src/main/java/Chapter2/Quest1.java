54d741b0b86e35bb320d4d4647502d86867efbec
SzymonW66 <szymekwajs@gmail.com>
2022-11-12 18:12:38 +0100

    Chapter2 complete

---
+++ src/main/java/Chapter2/Quest1.java
@@ -0,0 +1,12 @@
+package Chapter2;
+
+public class Quest1 {
+    public static void main(String[] args) {
+        String carMark = "BMW";
+        String carModel = "Series 8";
+        int carYear = 2000;
+        double carPice = 120000.00;
+
+        System.out.println(carMark+ " " + carModel + " " + carYear+ " " + carPice);
+    }
+}
