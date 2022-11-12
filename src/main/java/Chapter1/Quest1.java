fed96367fe55f1604b7227c1d2cf4690ffbafd72
SzymonW66 <szymekwajs@gmail.com>
2022-11-12 18:07:56 +0100

    Chapter1

---
+++ src/main/java/Chapter1/Quest1.java
@@ -0,0 +1,10 @@
+package Chapter1;
+
+public class Quest1 {
+    public static void main(String[] args) {
+        System.out.println("Część Jan słyszałem, że masz ");
+        System.out.print(25);
+        System.out.println(" lat i uczysz się programowania. Czy to prawda?");
+        System.out.print(true);
+    }
+}
